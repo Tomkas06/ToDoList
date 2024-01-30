82a198389c14d7e3eeb6a045782391950f332957
Tomkas <vecchitomas@gmail.com>
2024-01-30 20:40:29 -0300

    limpito

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function agregarTarea() {
         nuevaTareaDiv.appendChild(hechoNoHechoDiv);
 
         const actividadDiv = document.createElement('div');
-        actividadDiv.classList.add('col-md-6', "col-sm-6"); // Cambiado de 9 a 6
+        actividadDiv.classList.add('col-md-6', "col-sm-6", "tarea"); // Cambiado de 9 a 6
 
         const textoActividad = document.createElement('p');
         textoActividad.textContent = tareaTexto;
@@ -42,7 +42,7 @@ function agregarTarea() {
         botonEliminar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
       </svg>`;
-        botonEliminar.classList.add('col-md-3', "col-sm-3"); // Agregar clases de Bootstrap
+        botonEliminar.classList.add('col-md-3', "col-sm-3", "eliminar"); // Agregar clases de Bootstrap
 
         // Agregar evento para manejar el clic del botón de eliminación
         botonEliminar.addEventListener('click', function () {
